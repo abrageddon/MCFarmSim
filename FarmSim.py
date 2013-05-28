865d26eb2e90d18cc5c9c4e957887df9e2b5d6be
abrageddon <abrageddon@gmail.com>
2013-05-28 14:16:38 -0700

    step functions work

--- FarmSim.py
+++ FarmSim.py
@@ -5,8 +5,24 @@ Machine = SlaveMachine.SlaveMachine(3)
 
 Machine.step()
 print Machine.isDone()
+
+print "Reset machine; shoule be false"
+print Machine.startRun(30)#already running; cant reset till done
 Machine.step()
 print Machine.isDone()
 Machine.step()
+
+print "Done with one"
+print Machine.isDone()
+
+print "Start two"
+print Machine.startRun(3)
+Machine.step()
+print Machine.isDone()
+Machine.step()
+print Machine.isDone()
+Machine.step()
+
+print "Done with two"
 print Machine.isDone()
 
--- SlaveMachine.py
+++ SlaveMachine.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 class SlaveMachine:
-	description = """Slave machine that runs build jobs"""
-	timeOfJob = 0
-	timeRemaining = 0
-	def __init__(self, job):
-		self.timeOfJob = job
-		# +/- 3% or so?
-		self.timeRemaining = job
-	def step(self):
-		self.timeRemaining -= 1
-	def isDone(self):
-		return self.timeRemaining <= 0
+    description = """Slave machine that runs build jobs"""
+    timeRemaining = 0
+    def __init__(self, job):
+        # +/- 3% or so?
+        self.startRun(job)
+    def startRun(self, job):
+        if self.isDone():
+            self.timeRemaining = job
+            return True
+        return False
+    def step(self):
+        if not self.isDone():
+            self.timeRemaining -= 1
+    def isDone(self):
+        return self.timeRemaining <= 0
