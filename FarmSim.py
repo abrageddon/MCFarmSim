63ae14dd920b56c82fb1642daa47798cdc99e5a5
abrageddon <abrageddon@gmail.com>
2013-05-28 13:20:59 -0700

    making slave machine class

--- FarmSim.py
+++ FarmSim.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python
+import SlaveMachine
+
+Machine = SlaveMachine.SlaveMachine()
+
+Machine.Start()
---
+++ SlaveMachine.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+class SlaveMachine:
+    """Slave machine that runs build jobs"""
+    i = 12345
+    def Start(self):
+        print 'hello world'
+        return True
+
+def __init__(self):
+	pass
