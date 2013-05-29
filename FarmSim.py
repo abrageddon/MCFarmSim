6c49ef8fe3a4432c75eeebe98a28a467e7fcaf67
abrageddon <abrageddon@gmail.com>
2013-05-29 15:24:13 -0700

    basic linear simulation built

--- FarmSim.py
+++ FarmSim.py
@@ -1,70 +1,118 @@
 #!/usr/bin/env python
 import SlaveMachine, Storage
 
-numberOfSlaves = 19
+##### Setup
+numberOfSlaves = 20
 Slaves = []
 S3 = None
+currentStep = 0
+
+# 10,080 minutes in a week
+stepToStartTraffic = 10080
+
+# 100,000 is ok; 1,000,000 takes some time
+# 43,800 minutes in a month
+stepsInSim = 43800
+
 
 def main():
+
+
+    ##### Select traffic pattern
+    traffic = traConstantDemand
+
+    ##### Select algorthm
+    algorithm = algConstBuild
+
+    ##### Select setup
+    setup = setupFirefox_Naive_C_XL
+
+
+
+    ##### START
     setup()
 
-    #RUN ALGORITHM
+    global currentStep
+    currentStep = 0
 
-    for i in enumerate(10):
+    #RUN ALGORITHM
+    algorithm()
+
+    #WHILE TRAFFIC IS RUNNING
+    for i in range(stepsInSim):
         step()
+        #RUN TRAFFIC
+        S3.takeCopies(traffic())
         #RUN ALGORITHM
+        algorithm()
 
     #REPORT STATS
-    S3.printStats
+    print '=' * 80
+    print 'Total Cost of Slaves: ${0:.2f}'.format(totalSlaveCost())
+    print '=' * 80
+    S3.printStats()
+    print '=' * 80
+    print 'Total Cost: ${0:.2f}'.format(totalSlaveCost() + S3.totalCost)
+    print '=' * 80
 
-def setup():
-    S3 = Storage.Storage(1)
-    for i in enumerate(numberOfSlaves):
-        Slaves.append(SlaveMachine.SlaveMachine(0))
+
+##### Algorithms
+
+def algConstBuild():
+    global Slaves
+    for i in range(numberOfSlaves):
+        if Slaves[i].isDone():
+            Slaves[i].startRun(stepsToComplete)
+
+
+##### Traffics
+
+def traConstantDemand():
+    demand = 1
+    if currentStep > stepToStartTraffic:
+        return demand
+    return 0
+
+
+##### Setups
+
+def setupFirefox_Naive_C_XL():
+    global stepsToComplete
+    stepsToComplete = 22 # minutes
+    computeCost = 0.01 # $/min C1.XL
+    copiesPerGB = 33
+    generalSetup(stepsToComplete, computeCost, copiesPerGB)
+
+
+
+
+
+##### Simulation Functions
+def generalSetup(stepsToComplete, computeCost, copiesPerGB):
+    global S3
+    global Slaves
+    S3 = Storage.Storage(0,copiesPerGB) # copies per GB
+    for i in range(numberOfSlaves):
+        temp = SlaveMachine.SlaveMachine(0, computeCost)
+        Slaves.append(temp)
 
 def step():
+    global S3
+    global Slaves
+    global currentStep
+    currentStep += 1
     for slave in Slaves:
         slave.step()
         if slave.isDone():
-            S3.addCopies(1)
+            if slave.takeCopy():
+                S3.addCopies(1)
     S3.step()
-
-def test():
-    Machine = SlaveMachine.SlaveMachine(3)
-    S3 = Storage.Storage(1)
-
-    Machine.step()
-    Machine.isDone()
-
-    print "Trying to interrupt build; should be false: " + str(Machine.startRun(30))#already running; cant reset till done
-    Machine.step()
-    Machine.isDone()
-
-    Machine.step()
-    print "Done with one: " + str(Machine.isDone())
-    S3.addCopies(1)
-
-    S3.takeCopies(2)
-
-    print "Start two"
-    Machine.startRun(3)
-    Machine.step()
-    Machine.isDone()
-
-    S3.takeCopies(2)
-
-    Machine.step()
-    Machine.isDone()
-    Machine.step()
-
-    print "Done with two: " + str(Machine.isDone())
-    S3.addCopies(1)
-
-    S3.takeCopies(2)
-    S3.printStats()
-    print Machine.totalCost
+def totalSlaveCost():
+    totalCost = 0
+    for slave in Slaves:
+        totalCost += slave.totalCost
+    return totalCost
 
 
 if __name__ == '__main__':
-    # main()
-    test()
+    main()
--- SlaveMachine.py
+++ SlaveMachine.py
@@ -3,22 +3,28 @@
 class SlaveMachine:
     description = """Slave machine that runs build jobs"""
     timeRemaining = 0
-    costPerStep = 1
+    costPerStep = 0.1
     totalCost = 0
-    def __init__(self, job, cost=1):
+    def __init__(self, job, cost=0):
         # +/- 3% or so?
         self.startRun(job)
-        self.costPerStep = cost
+        if cost != 0:
+            self.costPerStep = cost
     def startRun(self, job):
-        if self.isDone():
+        if self.timeRemaining <= 0:
             self.timeRemaining = job
             return True
         return False
     def step(self):
-        if not self.isDone():
+        if self.timeRemaining > 0:
             self.timeRemaining -= 1
             self.totalCost += self.costPerStep
             return True
         return False
     def isDone(self):
         return self.timeRemaining <= 0
+    def takeCopy(self):
+        if self.timeRemaining == 0:
+            self.timeRemaining = -1
+            return True
+        return False
